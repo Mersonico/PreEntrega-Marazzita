cf8f175c4ef4f14df7941110f07af56e6af4d3b1
Critian Marazzita <cristianmarazzita@hotmail.com>
2023-02-24 18:38:59 -0300

    Parte 3

--- src/Components/CartWidget/CartWidget.jsx
+++ src/Components/CartWidget/CartWidget.jsx
@@ -5,7 +5,7 @@ import {MdOutlineAddShoppingCart} from "react-icons/md"
 const CartWidget = () => {
   return (
     <div>
-      
+      <span>0</span>
       <MdOutlineAddShoppingCart color="Blue" size= {25} />
     </div>
   )
