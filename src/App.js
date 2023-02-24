787b01689704b8770cc4fc48d4c7a0cffb7a15ce
Critian Marazzita <cristianmarazzita@hotmail.com>
2023-02-24 16:11:32 -0300

    Parte 1 Anda

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Footer from "./Components/Footer/Footer";
 import NavBar from "./Components/NavBar/NavBar";
-import ItemListContainer from "./Components/ItemListContainer/ItemListContainer";
+
 
 function App() {
   return (
     <div>
       <NavBar />
-      <ItemListContainer />
+   
       <Footer />
     
     </div>
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,14 +3,13 @@ import Logo from "../Logo/Logo";
 import styles from "./NavBar.module.css";
 
 const NavBar = () => {
-  
   return (
     <div className={styles.containerNavbar}>
       <Logo />
       <ul className={styles.containerList}>
-        <li style={{backgroundColor: "yellow"}}>Conocenos</li>
-        <li style={{backgroundColor: "yellow"}}>Canchas</li>
-        <li style={{backgroundColor: "yellow" }}>Ayuda</li>
+        <li style={{ backgroundColor: "yellow" }}>Conocenos</li>
+        <li style={{ backgroundColor: "yellow" }}>Canchas</li>
+        <li style={{ backgroundColor: "yellow" }}>Ayuda</li>
       </ul>
       <CartWidget />
     </div>
